023175c5953b45c890e86e0c27c73fd91a484960
Mustafa <mustafa@Mustafas-MacBook-Pro.local>
2019-04-02 22:21:28 -0700

    return the ractoral value for an input number

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,13 @@
 $(document).ready(function(){
   $("form").submit(function(event){
   var userInput = parseInt($("#number").val());
-  var total = 0;
-  console.log(typeof userInput);
-  for(var i=userInput; i>=1; i-=1){
-  total += (i * (i -1));
-  alert(total);
+  var total = 1;
+  for(index=1; index<=userInput; index++){
+  //alert(index);
+  total = (total * index);
 
 }
-  event.preventDefault();
+document.write("Factorial for " + userInput + " is " + total +".");
+event.preventDefault();
   });
 });
-
-
-// var iceCreamFlavors = ["mango","vanilla","berries"];
-//  for (var index = 0; index < iceCreamFlavors.length; index += 1) {
-//    document.write(iceCreamFlavors[index] + "<br>");
