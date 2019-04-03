75e6a7983cdb10be63a6e63d56a2bea610c0907e
Mustafa <mustafa@Mustafas-MacBook-Pro.local>
2019-04-02 22:29:57 -0700

    add if statement in a case if user enter number one or smaller

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,13 @@ $(document).ready(function(){
   $("form").submit(function(event){
   var userInput = parseInt($("#number").val());
   var total = 1;
+  if(userInput <= 1){
+    document.write(userInput + " doesn't have Factorial, please enter a number bigger than 1");
+  } else {
   for(index=1; index<=userInput; index++){
   //alert(index);
   total = (total * index);
-
+  }
 }
 document.write("Factorial for " + userInput + " is " + total +".");
 event.preventDefault();
